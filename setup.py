5228e50ee491181d3ce5f765d577ead8f47c18e0
sk2 <simon.knight@gmail.com>
2013-11-22 16:02:52 +1030

    initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup (
+     name = "autonetkit_vis",
+     version = "0.1.0",
+     description = 'Visualisation for AutoNetkit',
+     long_description = 'Visualisation for AutoNetkit',
+
+     author = 'Simon Knight',
+     author_email = "simon.knight@gmail.com",
+     url = "www.autonetkit.org",
+     packages = ['autonetkit_vis'],
+
+     include_package_data = True, # include data from MANIFEST.in
+
+     #package_data = {'': ['']},
+     download_url = (""),
+
+     install_requires = [''],
+
+     classifiers = [ ],     
+ 
+)
+
