205f39d01b6227817db0ff71cb0b107c69bdae2e
Simon Knight <simon.knight@gmail.com>
2014-03-13 16:05:34 +1030

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup (
      name = "autonetkit_vis",
-     version = "0.1.1",
+     version = "0.1.2",
      description = 'Visualisation for AutoNetkit',
      long_description = 'Visualisation for AutoNetkit',
 
